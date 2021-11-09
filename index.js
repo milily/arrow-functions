8369aa260da04f9713839ca0fab6e4a1438f4b4c
mrise <mrise@mrise.local>
2021-11-09 20:22:35 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const lechugaYPalta = () => {
+    const resultado = sandwichOrders.filter(sandwichOrder => 
+        sandwichOrder.ingredients.includes('lechuga') && 
+        sandwichOrder.ingredients.includes('palta')
+    )
+    return resultado
+}
+
+const encontrarOrdenPorId = idOrden => {
+
+    const filtrodeOrdenes = sandwichOrders.find(sandwichOrder => sandwichOrder.id === idOrden)
+    if(filtrodeOrdenes){
+        const{
+            ordered,
+            protein,
+            bread
+        } = filtrodeOrdenes
+    
+        const respuesta1 = `la orden fue realizada el ${ordered}, la orden llevó ${protein} y ${bread}`
+        return respuesta1
+        
+    }
+    
+    const respuesta2 = `No se encontró la orden con id ${idOrden}`
+    return respuesta2  
+}
+
+const ordenConPepinillos = idOrden => {
+
+    const filtrodeOrdenes = sandwichOrders.find(sandwichOrder => sandwichOrder.id === idOrden)
+
+    if(filtrodeOrdenes){
+        // const filtro = filtrodeOrdenes.ingredients.includes('pepinillos')
+        // return filtro
+        return filtrodeOrdenes.ingredients.some(ingredient => ingredient === 'pepinillos')
+    }
+}
+
+
