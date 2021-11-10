d0b2406cb7b0a22916084e45dfaf65736bd6efbf
mrise <mrise@mrise.local>
2021-11-10 18:50:45 -0300

    documenting

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * @function lechugaYPalta
+ * @returns {Array} returns an array containing all the orders with 'lechuga y palta'
+ */
 const lechugaYPalta = () => {
     const resultado = sandwichOrders.filter(sandwichOrder => 
         sandwichOrder.ingredients.includes('lechuga') && 
@@ -43,3 +47,75 @@ const ordenPorDia = fecha => {
     return `Se encontraron ${filtrodeOrdenes.length} para la fecha ${fecha}`
 }
 
+const ordenesConProteina = () =>{
+
+    const filtroProteinaYcebolla = sandwichOrders.filter(sandwichOrder => 
+        sandwichOrder.protein.includes('not burger') &&
+        sandwichOrder.ingredients.includes('cebolla caramelizada'))
+
+    return filtroProteinaYcebolla
+}
+
+const ordenesConReduce = () => {
+
+    const breads = ["bagette", "brioche", "pita", "marraqueta", "hallulla"]
+    const contadorPanes = {
+        hallulla: 0,
+        marraqueta: 0,
+        bagette: 0,
+        brioche: 0,
+        pita: 0
+    }
+
+    const tipoDePanes = sandwichOrders.reduce((totalPan, sandwichOrder) => {
+
+        // totalPan = {
+        //   hallulla: 0
+        //   ...  
+        // }
+        console.log(sandwichOrder.bread)
+        totalPan[sandwichOrder.bread] = totalPan[sandwichOrder.bread] + 1
+        return totalPan
+    },contadorPanes)
+
+    return tipoDePanes
+}
+
+const ejercicio = [1,2,2,1,4,5,7,10,10]
+const arraynuevo = ejercicio.reduce((posianterior, posiactual)=>{
+    if(posianterior.includes(posiactual)){
+        return posianterior
+    }
+    posianterior.push(posiactual)
+    return posianterior
+},[])
+
+
+
+const numeros = [1,2,3,4,5]
+
+const result = numeros.reduce((valorAnterior, valorActual) => {
+    // Primera iteracion
+    valorAnterior // 5
+    valorActual // 1
+    // return 5 + 1
+
+    // Segunda iteracion
+    valorAnterior // 6
+    valorActual // 2
+    // return 6 + 2
+
+    // Tercera iteracion
+    valorAnterior // 3
+    valorActual // 3
+    // return 3 + 3
+
+    // Tercera iteracion
+    valorAnterior // 6
+    valorActual // 4
+    // return 6 + 4
+
+    
+    const valorNuevo = valorAnterior + valorActual
+    return valorNuevo;
+}, 5)
